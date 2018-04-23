fe83ec3acc922d27676622319c578a8be71c24cc
newlyfei <newlyfei@163.com>
2018-04-23 18:27:57 +0800

    修改mvn关系

--- das-api/src/main/java/com/eyan/dasapi/DasApiApplication.java
+++ das-api/src/main/java/com/eyan/dasapi/DasApiApplication.java
@@ -2,8 +2,10 @@ package com.eyan.dasapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan(basePackages={"com.eyan","cn.hycun"})
 public class DasApiApplication {
 
 	public static void main(String[] args) {
